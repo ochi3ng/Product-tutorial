a657fcb0958df04fa992c7a1062feddddee44dbd
ochi3ng <johnochi3ng@gmail.com>
2022-07-25 10:22:36 +0300

    changes made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Product from './Component/Product';
+import Narvbar from './Component/Narvbar';
 
 function App() {
   return (
     <div className="App">
+      <Narvbar/>
    <Product/>
     </div>
   );
---
+++ src/Component/Narvbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+function Narvbar(){
+return(
+    <div className="Narvbar">
+        <h1 className="Navbar">
+            WELCOME TO OUR SHOP
+        </h1>
+    </div>
+)
+}
+export default Narvbar
